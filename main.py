2f23ddcab3e8bb3695cf4ea3442daa8515694489
emilblockovergaard <ebo007@live.dk>
2021-05-15 23:33:40 +0200

    Small clean-up: Removed old commented code.

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Expr:
         print("Im a undefined expr!")
 
     def evaluate(self):
-        return variable_dict[self.var_as_str]
+        return variable_dictionary[self.var_as_str]
         #return self.var_ref
 
     def add_child(self, child):
@@ -245,7 +245,7 @@ def string_to_node(string_in):
 if __name__ == '__main__':
 
     belief_base = []
-    variable_dict = {}
+    variable_dictionary = {}
     variables = VarTable()
     # First we try to implement expression: "(A&B)|C" into our code simply being able to represent it
     # Then we might add how to input a expression
@@ -257,7 +257,7 @@ if __name__ == '__main__':
     #str_in = "(A|B|C) & ~D" # Remove this
     #str_in = "(A|B|C) & (D<<G & D >>G)" # Remove this
     #str_in = "(A|B|C|D) & G & K & P & ~L & ~M, A | B" # Remove this
-    str_in = "C & (B | ~C)" # Remove this
+    str_in = "C & (B | ~C), C" # Remove this
     str_in = str(convert_print_to_cnf(str_in))
     str_cpy = str_in
     str_in = remove_exess(str_in)
@@ -273,30 +273,12 @@ if __name__ == '__main__':
             print("it's a not")
             i+=1
             print(test_str[i])
-            variable_dict[test_str[i]] = False
+            variable_dictionary[test_str[i]] = False
         else:
-            variable_dict[test_str[i]] = True
+            variable_dictionary[test_str[i]] = True
         i +=1
 
-    print(variable_dict)
-    # Find pairs of "(" and ")"
-    # Test string: ((A&B)&(C|B))&(A|B)      CNF form ->     A & B & (C|B) & (A|B)
-    # (A&B)&C -> A&B&C
-    #
-    # pair_list = []
-    #
-    # left_par = 0
-    # right_par = 0
-    # for chara in str_in:
-    #     if chara == '(':
-    #         left_par += 1
-    #     if chara == ')':
-    #         right_par += 1
-    #
-    # if left_par != right_par:
-    #     print("Error in input not equal amount of parenthesis!")
-    # else:
-    #     print("Left par: %d \t Right par: %d" % (left_par, right_par))
+    print(variable_dictionary)
 
     beliefs = str_in.split(",")
     print(beliefs)
@@ -309,15 +291,6 @@ if __name__ == '__main__':
         for x in range(0, len(andElems)):
             orElems.append(andElems[x].split("|"))
 
-        # #variables.print_elems()
-        # for i in range(0, len(orElems)):
-        #     #print("Subsub print: " + str(orElems[i]))
-        #     for element in orElems[i]:
-        #         print("variables: " + element)
-        #         #variables.add_elem(element)
-        #
-        # #variables.print_elems()
-
         # Create a tree from OR
         or_head = None
         not_head = None
@@ -329,7 +302,6 @@ if __name__ == '__main__':
 
         # Loop through the list of lists
         for x in orElems:
-
             # If it only contains one member we create a node from this:
             if len(x) == 1:
                 # Add to the list of and heads
@@ -346,29 +318,6 @@ if __name__ == '__main__':
                 and_heads.append(create_or_head(or_heads))
 
 
-        # for x in orElems:
-        #     print(x)
-        #     if x[0][0]== '~':
-        #         print("It's a NOT expr!")
-        #         not_head = NotExpr()
-        #         create_not_expr(not_head, x)
-        #         and_head.add_child(not_head)
-        #         and_heads.append(not_head)
-        #     else:
-        #         if len(x) == 1:
-        #             print("It's a single expression!")
-        #             new_expr = Expr()
-        #             new_expr.var_as_str = x[0]
-        #             x.pop(0)
-        #             and_heads.append(new_expr)
-        #         else:
-        #             print("It's a OR expr!")
-        #             or_head = OrExpr()
-        #             create_or_expr(or_head, x)
-        #             or_head.print_tree(False)
-        #             and_head.add_child(or_head)
-        #             and_heads.append(or_head)
-
         print(orElems)
 
         print("\n\nPrinting and_heads: ")
@@ -376,15 +325,9 @@ if __name__ == '__main__':
         total_tree.print_tree()
         belief_base.append(total_tree)
 
-    print(variable_dict)
+    print(variable_dictionary)
     for i in range(0, len(belief_base)):
         print("Testing the " + str(i) + "th belief: " + str(belief_base[i].evaluate()))
-
-
-
-
-
-
 
 
     # print("Left par: %d \t Right par: %d" % (first_right, first_left))
